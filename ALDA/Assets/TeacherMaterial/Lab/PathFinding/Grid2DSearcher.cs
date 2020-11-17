ef59dc9eb60aa63609a052edfcd67ee784c5b239
Flargy <marlun1996@hotmail.com>
2020-11-17 10:13:10 +0100

    teacher material added

---
+++ ALDA/Assets/TeacherMaterial/Lab/PathFinding/Grid2DSearcher.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FutureGames.Lab
+{
+    public class Grid2DSearcher
+    {
+        GridConnected2D grid = null;
+
+        float animationWait = 0.01f;
+
+        // key: cell going to
+        // value: cell comming from
+        Dictionary<GridCell2D, GridCell2D> cameFrom = new Dictionary<GridCell2D, GridCell2D>();
+
+        public Grid2DSearcher(GridConnected2D grid)
+        {
+            this.grid = grid;
+
+            //GridCell2D.BecomeTarget += SetTarget;
+        }
+
+        //public IEnumerator BreadthFirstTravel(GridCell2D start)
+        //{
+        //    this.start = start;
+
+        //    Queue<GridCell2D> frontier = new Queue<GridCell2D>();
+        //    HashSet<GridCell2D> reached = new HashSet<GridCell2D>();
+
+        //    frontier.Enqueue(start);
+        //    reached.Add(start);
+
+        //    while(frontier.Count > 0)
+        //    {
+        //        GridCell2D current = frontier.Dequeue();
+        //        foreach(GridCell2D t in current.Neighbors)
+        //        {
+        //            if (reached.Contains(t))
+        //                continue;
+
+        //            frontier.Enqueue(t);
+        //            reached.Add(t);
+        //        }
+
+        //        foreach (GridCell2D t in reached)
+        //        {
+        //            t.SetColor(Color.white);
+        //        }
+
+        //        foreach (GridCell2D t in frontier)
+        //        {
+        //            t.SetColor(GridCell2D.frontierColor);
+        //        }
+
+        //        yield return new WaitForSeconds(animationWait);
+        //    }
+        //}
+
+        public IEnumerator BreadthFirstTravelPathTrack()
+        {
+            Queue<GridCell2D> frontier = new Queue<GridCell2D>();
+            cameFrom.Clear();
+
+            frontier.Enqueue(grid.StartCell);
+            cameFrom.Add(grid.StartCell, null);
+
+            while (frontier.Count > 0)
+            {
+                GridCell2D current = frontier.Dequeue();
+                foreach (GridCell2D t in current.Neighbors)
+                {
+                    if (t.IsWall())
+                        continue;
+
+                    if (cameFrom.ContainsKey(t))
+                        continue;
+
+                    frontier.Enqueue(t);
+                    cameFrom.Add(t, current);
+                }
+
+                if (current == grid.TargetCell)
+                    break;
+
+                foreach (GridCell2D t in cameFrom.Keys)
+                {
+                    GridCell2D value = cameFrom[t];
+                    if (value == null)
+                        continue;
+                    value.SetColor(Color.white);
+                }
+
+                foreach (GridCell2D t in frontier)
+                {
+                    t.SetColor(GridCell2D.frontierColor);
+                }
+
+                yield return new WaitForSeconds(animationWait);
+            }
+        }
+
+        public void Run()
+        {
+            DrawPathToTarget();
+        }
+
+        private void DrawPathToTarget()
+        {
+            if (grid.TargetCell == null)
+                return;
+
+            GridCell2D current = grid.TargetCell;
+
+            while (current != grid.StartCell)
+            {
+                if (cameFrom.ContainsKey(current) == false)
+                    break;
+
+                current = cameFrom[current];
+                current.SetColor(GridCell2D.pathColor);
+            }
+        }
+    }
+}
---
+++ ALDA/Assets/TeacherMaterial/Lab/PathFinding/GridCell2D.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FutureGames.Lab
+{
+    //public enum MouseState
+    //{
+    //    None,
+    //    Hover,
+    //    NeighborOfHover,
+    //}
+
+    public enum WalkState
+    {
+        Start,
+        Target,
+        Walkable,
+        Wall,
+    }
+
+    public class GridCell2D
+    {
+        public static Action<GridCell2D> BecomeTarget = delegate { };
+
+        GridConnected2D grid;
+        Vector2Int index = Vector2Int.zero;
+
+        List<GridCell2D> neighbors = new List<GridCell2D>();
+        public List<GridCell2D> Neighbors => neighbors;
+
+        GridCell2DMono mono = null;
+
+
+        public static Color startColor = Color.black;
+        public static Color defaultColor = Color.grey;
+
+        public static Color wallColor = Color.red;
+
+        public static Color targetColor = Color.green;
+        public static Color frontierColor = Color.blue;
+
+        public static Color pathColor = Color.yellow;
+
+        bool fourNeibs = false;
+
+        //MouseState mouseState = MouseState.None;
+
+        WalkState walkState = WalkState.Walkable;
+
+        public GridCell2D(GridConnected2D grid, Vector2Int index, GridCell2DMono mono, WalkState walkState)
+        {
+            this.grid = grid;
+            this.index = index;
+
+            this.mono = mono;
+            this.mono.cell = this;
+
+            SetColor(defaultColor);
+
+            this.walkState = walkState;
+            switch(walkState)
+            {
+                case WalkState.Start:
+                    mono.SetColor(startColor);
+                    break;
+
+                case WalkState.Target:
+                    mono.SetColor(targetColor);
+                    break;
+
+                case WalkState.Walkable:
+                    break;
+
+                case WalkState.Wall:
+                    mono.SetColor(wallColor);
+                    break;
+            }
+        }
+
+        public void Run()
+        {
+            //ColorOnMouseState();
+
+            //SetNeighborsMouseState();
+        }
+
+        //private void SetNeighborsMouseState()
+        //{
+        //    switch(mouseState)
+        //    {
+        //        case MouseState.None:
+        //            //SetNeiborsMouseState(MouseState.None);
+        //            break;
+
+        //        case MouseState.Hover:
+        //            SetNeiborsMouseState(MouseState.NeighborOfHover);
+        //            break;
+
+        //        case MouseState.NeighborOfHover:
+        //            break;
+        //    }    
+        //}
+
+        public void SetMeAsTarget()
+        {
+            BecomeTarget(this);
+        }
+
+        public void SetColor(Color color)
+        {
+            if (walkState == WalkState.Wall || walkState == WalkState.Start || walkState == WalkState.Target)
+                return;
+
+            mono.SetColor(color);
+        }
+
+        public bool IsWall()
+        {
+            return walkState == WalkState.Wall;
+        }
+
+        //public void SetNeiborsMouseState(MouseState state)
+        //{
+        //    foreach(GridCell2D t in neighbors)
+        //    {
+        //        t.SetMouseState(state);
+        //    }
+        //}
+
+        //public void SetMouseState(MouseState newState)
+        //{
+        //    mouseState = newState;
+        //}
+
+        //private void ColorOnMouseState()
+        //{
+        //    switch(mouseState)
+        //    {
+        //        case MouseState.None:
+        //            break;
+
+        //        case MouseState.Hover:
+        //            SetColor(hoverColor);
+        //            break;
+
+        //        case MouseState.NeighborOfHover:
+        //            break;
+        //    }
+        //}
+
+        public void StoreNeighbors()
+        {
+            neighbors.Clear();
+            if (IsDownLimit() && !IsDownLeftLimit() && !IsDownRightLimit())
+            {
+                neighbors.Add(grid.Cells[index.x - 1, index.y]);
+                
+                if(fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y + 1]);
+                
+                neighbors.Add(grid.Cells[index.x, index.y + 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y + 1]);
+
+                neighbors.Add(grid.Cells[index.x + 1, index.y]);
+            }
+            else if (IsUpLimit() && !IsUpLeftLimit() && !IsUpRightLimit())
+            {
+                neighbors.Add(grid.Cells[index.x - 1, index.y]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y - 1]);
+                neighbors.Add(grid.Cells[index.x, index.y - 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y - 1]);
+
+                neighbors.Add(grid.Cells[index.x + 1, index.y]);
+            }
+
+            else if (IsLeftLimit() && !IsDownLeftLimit() && !IsUpLeftLimit())
+            {
+                neighbors.Add(grid.Cells[index.x, index.y - 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y - 1]);
+
+                neighbors.Add(grid.Cells[index.x + 1, index.y]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y + 1]);
+
+                neighbors.Add(grid.Cells[index.x, index.y + 1]);
+            }
+            else if (IsRightLimit() && !IsDownRightLimit() && !IsUpRightLimit())
+            {
+                neighbors.Add(grid.Cells[index.x, index.y - 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y - 1]);
+
+                neighbors.Add(grid.Cells[index.x - 1, index.y]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y + 1]);
+
+                neighbors.Add(grid.Cells[index.x, index.y + 1]);
+            }
+
+            else if (IsDownLeftLimit())
+            {
+                neighbors.Add(grid.Cells[index.x, index.y + 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y + 1]);
+
+                neighbors.Add(grid.Cells[index.x + 1, index.y]);
+            }
+            else if (IsUpLeftLimit())
+            {
+                neighbors.Add(grid.Cells[index.x, index.y - 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y - 1]);
+                
+                neighbors.Add(grid.Cells[index.x + 1, index.y]);
+            }
+
+            else if (IsDownRightLimit())
+            {
+                neighbors.Add(grid.Cells[index.x, index.y + 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y + 1]);
+
+                neighbors.Add(grid.Cells[index.x - 1, index.y]);
+            }
+            else if (IsUpRightLimit())
+            {
+                neighbors.Add(grid.Cells[index.x, index.y - 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y - 1]);
+
+                neighbors.Add(grid.Cells[index.x - 1, index.y]);
+            }
+
+            else
+            {
+                neighbors.Add(grid.Cells[index.x - 1, index.y]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y + 1]);
+
+                neighbors.Add(grid.Cells[index.x, index.y + 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y + 1]);
+
+                neighbors.Add(grid.Cells[index.x + 1, index.y]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x - 1, index.y - 1]);
+
+                neighbors.Add(grid.Cells[index.x, index.y - 1]);
+
+                if (fourNeibs == false)
+                    neighbors.Add(grid.Cells[index.x + 1, index.y - 1]);
+            }
+        }
+
+        bool IsDownLimit()
+        {
+            return index.y == 0;
+        }
+
+        bool IsUpLimit()
+        {
+            return index.y == grid.Height - 1;
+        }
+
+        bool IsLeftLimit()
+        {
+            return index.x == 0;
+        }
+
+        bool IsRightLimit()
+        {
+            return index.x == grid.Width - 1;
+        }
+
+        bool IsDownLeftLimit()
+        {
+            return IsDownLimit() && IsLeftLimit();
+        }
+
+        bool IsUpLeftLimit()
+        {
+            return IsUpLimit() && IsLeftLimit();
+        }
+
+        bool IsUpRightLimit()
+        {
+            return IsUpLimit() && IsRightLimit();
+        }
+
+        bool IsDownRightLimit()
+        {
+            return IsDownLimit() && IsRightLimit();
+        }
+    }
+}
